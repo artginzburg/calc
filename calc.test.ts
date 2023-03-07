4b94fb511fc78c4206693678cf92fe53312a4e59
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-03-07 09:41:32 +0400

    use Jest for testing

---
+++ calc.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, test } from '@jest/globals';
+
+import calc from './calc';
+
+describe('calc', () => {
+  test('calculates', () => {
+    expect(calc('-243')).toBe(-243);
+    expect(calc('(-243)')).toBe(-243);
+    expect(calc('-(243)')).toBe(-243);
+    expect(calc('-(-243)')).toBe(243);
+    expect(calc('-(-2+3)')).toBe(-1);
+    expect(calc('(24)-3')).toBe(21);
+    expect(calc('-3-4*2')).toBe(-11);
+    expect(calc('3^4*2')).toBe(162);
+    expect(calc('3+4^2')).toBe(19);
+    expect(calc('3+-(4-2)^2')).toBe(-1);
+    expect(calc('3+(-4-2)^2')).toBe(39);
+    expect(calc('(-3-4)*2')).toBe(-14);
+    expect(calc('-(-3-4)*2')).toBe(14);
+    expect(calc('-(-3-4)*-2')).toBe(-14);
+    expect(calc('-3-4*2*3-5+2')).toBe(-30);
+    expect(calc('((3*(3-4))+2)*3')).toBe(-3);
+    expect(calc('((3.53*(3.02-4.5))+2.88)*3.307')).toBeCloseTo(-7.7529308, 14);
+    expect(calc('3.53*3.02-4.5+2.88*3.307')).toBeCloseTo(15.68476, 14);
+    expect(calc('1/2')).toBe(0.5);
+    expect(calc('1/-2')).toBe(-0.5);
+    expect(calc('1-1')).toBe(0);
+  });
+});
---
+++ jest.config.cjs
@@ -0,0 +1,15 @@
+/** @see https://github.com/facebook/jest/blob/654949b5897c93cc3d7cf5d9c0efbb45c21a8c45/packages/jest-config/src/Defaults.ts#L28 */
+const defaultCoverageReporters = require('jest-config').defaults.coverageReporters;
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  // #region TypeScript Jest essentials (`ts-jest config:init`)
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  // #endregion
+
+  collectCoverage: true,
+  coverageReporters: [...defaultCoverageReporters, 'json-summary'],
+
+  testPathIgnorePatterns: ['dist'],
+};
